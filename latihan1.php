5a037d110c1860527736bb33a7f58e7ed6668d68
faulah <rohmahfaulah@gmail.com>
2024-05-01 08:21:30 +0700

    h

---
+++ latihan1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Div dan Span</title>
+    <link rel="icon" type="img/png" href="gambar/icon.png" sizes="16x16">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Belajar PHP">
+    <meta name="keywords" content="233307053">
+    <meta name="author" content="Khoirul Faulah Nr">
+</head>
+<body>
+    <h1>Halaman PHP Saya</h1>
+    <?php
+    echo "Hallo ini halaman dengan menggunakan bahasa PHP!";
+
+    ?>
+</body>
+</html>
